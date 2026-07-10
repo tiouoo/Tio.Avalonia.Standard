f2298a51ecee8c7eba2e2961d384413887c4e7f2
tiouoo <tiouo@qq.com>
2026-07-11 07:49:59 +0800

    feat(extensions): 添加 ObservableCollection 扩展方法
    - 实现了 AddRange 方法用于批量添加元素到 ObservableCollection
    - 提供了泛型支持以增强类型安全性
    - 简化了集合操作的代码实现

---
+++ src/Tio.Avalonia.Standard/Modules/Extensions/List.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Tio.Avalonia.Standard.Modules.Extensions;
+
+public static class List
+{
+    public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            collection.Add(item);
+        }
+    }
+}
