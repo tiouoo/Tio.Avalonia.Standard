b9372b8977a5d129ab3cd7322c1310e2ac46176c
Tt <tiouo@qq.com>
2026-07-11 18:06:36 +0800

    feat(tab): 添加顶级窗口扩展方法支持
    - 实现 TryGetToast 方法获取 Toast 管理器
    - 实现 TryGetNotification 方法获取通知管理器
    - 集成 TioWindow 和 TioTabWindowBase 类型转换
    - 提供控制组件的顶层窗口访问能力

---
+++ src/Tio.Avalonia.Standard.Tab/Extensions/TopLevel.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Tio.Avalonia.Standard.Tab.Interface;
+using TioUi.Common.Extensions;
+using TioUi.Controls;
+
+namespace Tio.Avalonia.Standard.Tab.Extensions;
+
+public static class TopLevel
+{
+    public static TioToastManager? TryGetToast(this Control control)
+    {
+        return ((control.GetTopLevel() as TioWindow) as TioTabWindowBase)?.Toast;
+    }
+
+    public static TioNotificationManager? TryGetNotification(this Control control)
+    {
+        return ((control.GetTopLevel() as TioWindow) as TioTabWindowBase)?.Notification;
+    }
+}
