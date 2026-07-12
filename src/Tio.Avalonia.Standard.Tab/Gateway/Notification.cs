b4961c307d878156d5925b7b062a7978ca97ac1d
Tt <tiouo@qq.com>
2026-07-12 21:21:52 +0800

    feat(notification): 添加通知网关实现
    - 新增 NotificationGateway 类用于统一管理通知功能
    - 实现基于 Toast 和 Notification 的双重通知机制
    - 提供三个重载方法支持不同类型的通知显示
    - 添加 IsToastFunc 静态函数用于判断是否使用 Toast 模式
    - 集成 Avalonia 控件系统的通知接口
    - 实现通知类型的条件判断和相应展示逻辑

---
+++ src/Tio.Avalonia.Standard.Tab/Gateway/Notification.cs
@@ -0,0 +1,49 @@
+﻿using Avalonia.Controls.Notifications;
+using Tio.Avalonia.Standard.Tab.Extensions;
+using TioUi.Common.Classes;
+using TopLevel = Avalonia.Controls.TopLevel;
+
+namespace Tio.Avalonia.Standard.Tab.Gateway;
+
+public class NotificationGateway
+{
+    public static Func<bool> IsToastFunc = null;
+    
+    public static void Notice(TopLevel topLevel, NotificationOptions options)
+    {
+        if (IsToastFunc())
+        {
+            topLevel.TryGetToast()?.Show(options);
+        }
+        else
+        {
+            topLevel.TryGetNotification()?.Show(options);
+        }
+    }
+
+    public static void Notice(TopLevel topLevel, string msg, NotificationType type = NotificationType.Information)
+    {
+        if (IsToastFunc())
+        {
+            topLevel.TryGetToast()?.Show(msg, type);
+        }
+        else
+        {
+            topLevel.TryGetNotification()?.Show("Portal", msg, new NotificationOptions { Type = type });
+        }
+    }
+
+
+    public static void Notice(TopLevel topLevel, string msg, string title,
+        NotificationType type = NotificationType.Information)
+    {
+        if (IsToastFunc())
+        {
+            topLevel.TryGetToast()?.Show(msg, type);
+        }
+        else
+        {
+            topLevel.TryGetNotification()?.Show(title, msg, new NotificationOptions { Type = type });
+        }
+    }
+}
