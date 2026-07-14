d3d7b216c5352629f5bc4d06b899294ac0895602
tiouoo <tiouo@qq.com>
2026-07-14 21:54:57 +0800

    feat(converters): 添加字符包装转换器实现
    - 实现了 CharacterWrapConverter 类继承 IValueConverter 接口
    - 在 Convert 方法中将字符串转换为零宽空格分隔的字符序列
    - 对空值或空字符串进行保护性检查并直接返回原值
    - ConvertBack 方法抛出 NotSupportedException 异常表示不支持反向转换

---
+++ src/Tio.Avalonia.Standard/Modules/Converters/CharacterWrapConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Tio.Avalonia.Standard.Modules.Converters;
+
+public class CharacterWrapConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string text && !string.IsNullOrEmpty(text))
+        {
+            return string.Join("\u200B", text.ToCharArray());
+        }
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
